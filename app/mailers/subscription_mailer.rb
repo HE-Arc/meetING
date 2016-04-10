2ad7c3be156f9280722d7ca1c0fbd9b95a189d56
Horia Mut <horia.mut@net2000.ch>
2016-04-11 00:04:19 +0200

    Fix problem with delivery options. They cause an getAddrInfo error, service nor recognized.

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -5,12 +5,8 @@ class SubscriptionMailer < ApplicationMailer
     @post = post
     @post_author = post_author
 
-    # Should not need to be done here as it was set up in the base class.
-    # This is just to be sure.
-    delivery_options = { address: "noreply@meetING.com" }
-
     Rails.logger.info "===========SENDING MATCH EMAIL TO ========> #{@email}"
 
-    mail(to: @email, subject: @subject, delivery_method_options: delivery_options)
+    mail(to: @email, subject: @subject)
   end
 end

82cfc467cae5d00d3ec3a7d566a4c6d6c7a54de8
Jeshon Assunçao <jeshon.assuncao@he-arc.ch>
2016-04-11 09:53:05 +0200

    Trying to deploy ...

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'srvz-webapp.he-arc.ch', user: 'jeremy', roles: %w{app db web}
+server 'srvz-webapp.he-arc.ch', user: 'jeshon', roles: %w{app db web}
 
 
 # role-based syntax

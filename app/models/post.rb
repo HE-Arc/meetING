fbccd5beeb01783363e488651a09511e16568ea7
Wirth <wirjy93@gmail.com>
2016-04-17 17:57:05 +0200

    remis le card large car sinon il y a des trous dans la grille adapté la page d'admin pour être conforme a la page d'acceuil

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   has_attached_file :image,
   :styles => {
   :blur => ""},
-  :convert_options => { :blur => "-blur 0x50" }
+  :convert_options => { :blur => "-blur 0x40" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end 
